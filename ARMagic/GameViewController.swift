bf028ac18b4757690384e46873c410c9f45c9068
Derek_Han <donglak.han@gmail.com>
2018-05-30 13:26:32 -0500

    add extension

--- ARMagic/GameViewController.swift
+++ ARMagic/GameViewController.swift
@@ -9,7 +9,7 @@
 import ARKit
 import LBTAComponents
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, ARSCNViewDelegate {
     
     let arView: ARSCNView = {
         let view = ARSCNView()
@@ -33,9 +33,30 @@ class GameViewController: UIViewController {
     
     @objc func handlePlusButtonTapped() {
         print("Tapped on plus button")
-        addBox()
+        addNode()
         
     }
+    
+    let resetButtonWidth = ScreenSize.width * 0.1
+    lazy var resetButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "reset").withRenderingMode(.alwaysTemplate), for: .normal)
+        button.tintColor = UIColor(white: 1.0, alpha: 0.7)
+//        button.layer.cornerRadius = resetButtonWidth * 0.5
+//        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(handleResetButtonTapped), for: .touchUpInside)
+        // Safe way to use img
+        button.layer.zPosition = 1
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }()
+    
+    @objc func handleResetButtonTapped() {
+        print("Tapped on reset button")
+        resetScene()
+        
+    }
+    
     
     let minusButtonWidth = ScreenSize.width * 0.1
     lazy var minusButton: UIButton = {
@@ -65,14 +86,16 @@ class GameViewController: UIViewController {
         
         setupViews()
         
+        configuration.planeDetection = .horizontal
 
 
         arView.session.run(configuration, options: [])
         // yellow little dots. information from the camera
         arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         arView.autoenablesDefaultLighting = true
-        
-}
+        arView.delegate = self
+        }
+    
     override var prefersStatusBarHidden: Bool{
         return true
     }
@@ -89,37 +112,67 @@ class GameViewController: UIViewController {
         arView.fillSuperview()
         
         view.addSubview(plusButton)
-        plusButton.anchor(nil, left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: nil, topConstant: 0, leftConstant: 24, bottomConstant: 12, rightConstant: 0, widthConstant: plusButtonWidth, heightConstant: plusButtonWidth)
+        plusButton.anchor(nil, left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: nil, topConstant: 0, leftConstant: 24, bottomConstant: 24, rightConstant: 0, widthConstant: plusButtonWidth, heightConstant: plusButtonWidth)
         
         view.addSubview(minusButton)
-        minusButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 12, rightConstant: 24, widthConstant: minusButtonWidth, heightConstant: minusButtonWidth)
+        minusButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 24, rightConstant: 24, widthConstant: minusButtonWidth, heightConstant: minusButtonWidth)
         
+        view.addSubview(resetButton)
+        resetButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 24, rightConstant: 0, widthConstant: resetButtonWidth, heightConstant: resetButtonWidth)
+        resetButton.anchorCenterXToSuperview()
     }
     
-    func addBox() {
-        let boxNode = SCNNode()
-        boxNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0002)
-        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
-        // 0.3 = 30cm
-        boxNode.position = SCNVector3(Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5) )
-        boxNode.name = "box"
-        arView.scene.rootNode.addChildNode(boxNode)
+    func addNode() {
+        let shapeNode = SCNNode()
+//        boxNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0002)
+//        shapeNode.geometry = SCNCapsule(capRadius: 0.05, height: 0.20)
+//        shapeNode.geometry = SCNCone(topRadius: 0.05, bottomRadius: 0.10, height: 0.15)
+//        shapeNode.geometry = SCNCylinder(radius: 0.15, height: 0.15)
+        shapeNode.geometry = SCNTorus(ringRadius: 0.15, pipeRadius: 0.05)
+
+        shapeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        // 0.3 = 30cm, random position
+        shapeNode.position = SCNVector3(Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5) )
+//        shapeNode.position = SCNVector3(0.0, 0.0, -0.3)
+        shapeNode.name = "node"
+        arView.scene.rootNode.addChildNode(shapeNode)
     }
     
     func removeAllBoxes() {
         arView.scene.rootNode.enumerateChildNodes { (node, _) in
-            if node.name == "box"{
+            if node.name == "node"{
                 node.removeFromParentNode()
             }
         
         }
     }
     
+    func resetScene() {
+        arView.session.pause()
+        arView.scene.rootNode.enumerateChildNodes { (node, _) in
+            if node.name == "node" {
+                node.removeFromParentNode()
+            }
+        }
+        arView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
+    
+    }
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let anchorPlane = anchor as? ARPlaneAnchor else { return }
+        print("New Plane Anchor found at extent:", anchorPlane.extent)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let anchorPlane = anchor as? ARPlaneAnchor else { return }
+        print("Plane Anchor updated with extent:", anchorPlane.extent)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let anchorPlane = anchor as? ARPlaneAnchor else { return }
+        print("Plane Anchor removed with extent:", anchorPlane.extent)
+    }
+    
 }
-
-
-
-
 
 
 
